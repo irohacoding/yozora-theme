c8dc91821bbab302494b58495f1a3f3572b20fd5
irohacoding <info@irohacoding.com>
2023-02-15 11:02:18 +0900

    Add vertical-border face and align source code

--- yozora-theme.el
+++ yozora-theme.el
@@ -30,11 +30,6 @@
 ;; Yozora theme is a custom theme for GNU Emacs. Inspired by moon sky at night.
 ;; Yozora means night sky in Japanese.
 
-;; Special Thanks
-
-;; All the themes for GNU Emacs!!! Always refer to these when making this theme!!
-;; Thank you very much.
-
 ;;; Code:
 
 (deftheme yozora
@@ -71,68 +66,72 @@ The theme has to be reloaded after changing anything in this group."
 
   (custom-theme-set-faces
    'yozora
-   `(default ((t (:foreground ,yozora-wing :background ,yozora-sky :bold nil))))
-   `(cursor ((t (:background ,yozora-hero))))
-   `(escape-glyph ((t (:foreground ,yozora-leaf))))
-   `(homoglyph ((t (:foreground ,yozora-leaf))))
-   `(minibuffer-prompt ((t (:foreground ,yozora-leaf))))
-   `(highlight ((t (:background ,yozora-petal))))
-   `(region ((t (:extend t :background ,yozora-cloud))))
-   `(shadow ((t (:foreground ,yozora-meteor))))
+
+   `(default             ((t (:foreground ,yozora-wing :background ,yozora-sky :bold nil))))
+   `(cursor              ((t (:background ,yozora-hero))))
+   `(escape-glyph        ((t (:foreground ,yozora-leaf))))
+   `(homoglyph           ((t (:foreground ,yozora-leaf))))
+   `(minibuffer-prompt   ((t (:foreground ,yozora-leaf))))
+   `(highlight           ((t (:background ,yozora-petal))))
+   `(region              ((t (:extend t :background ,yozora-cloud))))
+   `(shadow              ((t (:foreground ,yozora-meteor))))
    `(secondary-selection ((t (:extend t :foreground ,yozora-bird :background ,yozora-star))))
    `(trailing-whitespace ((t (:foreground ,yozora-bird :background ,yozora-star))))
-   `(show-paren-match ((t (:foreground ,yozora-bird :background ,yozora-star))))
-   `(show-paren-match-expression ((t (:background ,yozora-cloud))))
-   `(show-paren-mismatch ((t (:background ,yozora-heart))))
 
-   `(font-lock-builtin-face ((t (:foreground ,yozora-moon))))
-   '(font-lock-comment-delimiter-face ((default (:inherit (font-lock-comment-face)))))
-   `(font-lock-comment-face ((t (:foreground ,yozora-star))))
-   `(font-lock-constant-face ((t (:foreground ,yozora-leaf))))
-   '(font-lock-doc-face ((t (:inherit (font-lock-string-face)))))
-   `(font-lock-function-name-face ((t (:foreground ,yozora-star))))
-   `(font-lock-keyword-face ((t (:foreground ,yozora-heart))))
-   '(font-lock-negation-char-face ((t nil)))
-   '(font-lock-preprocessor-face ((t (:inherit (font-lock-builtin-face)))))
+   `(show-paren-match            ((t (:foreground ,yozora-bird :background ,yozora-star))))
+   `(show-paren-match-expression ((t (:background ,yozora-cloud))))
+   `(show-paren-mismatch         ((t (:background ,yozora-heart))))
+
+   `(font-lock-builtin-face              ((t (:foreground ,yozora-moon))))
+   '(font-lock-comment-delimiter-face    ((default (:inherit (font-lock-comment-face)))))
+   `(font-lock-comment-face              ((t (:foreground ,yozora-star))))
+   `(font-lock-constant-face             ((t (:foreground ,yozora-leaf))))
+   '(font-lock-doc-face                  ((t (:inherit (font-lock-string-face)))))
+   `(font-lock-function-name-face        ((t (:foreground ,yozora-star))))
+   `(font-lock-keyword-face              ((t (:foreground ,yozora-heart))))
+   '(font-lock-negation-char-face        ((t nil)))
+   '(font-lock-preprocessor-face         ((t (:inherit (font-lock-builtin-face)))))
    '(font-lock-regexp-grouping-backslash ((t (:inherit (bold)))))
    '(font-lock-regexp-grouping-construct ((t (:inherit (bold)))))
-   `(font-lock-string-face ((t (:foreground ,yozora-star))))
-   `(font-lock-type-face ((t (:foreground ,yozora-meteor))))
-   `(font-lock-variable-name-face ((t (:foreground ,yozora-leaf))))
-   '(font-lock-warning-face ((t (:inherit (error)))))
+   `(font-lock-string-face               ((t (:foreground ,yozora-star))))
+   `(font-lock-type-face                 ((t (:foreground ,yozora-meteor))))
+   `(font-lock-variable-name-face        ((t (:foreground ,yozora-leaf))))
+   '(font-lock-warning-face              ((t (:inherit (error)))))
 
    `(mode-line ((t (:foreground ,yozora-wing :background ,yozora-insect
                                 :box (:line-width -1 :color ,yozora-insect) :weight normal))))
    '(mode-line-buffer-id ((t (:weight bold))))
-   '(mode-line-emphasis ((t (:weight bold))))
+   '(mode-line-emphasis  ((t (:weight bold))))
    `(mode-line-highlight ((t (:box (:line-width 2 :color ,yozora-insect)))))
-   `(mode-line-inactive ((t (:inherit mode-line :background ,yozora-bird :foreground ,yozora-insect :weight light))))
+   `(mode-line-inactive  ((t (:inherit mode-line :background ,yozora-bird
+                                       :foreground ,yozora-insect :weight light))))
+   `(vertical-border     ((t (:foreground ,yozora-insect))))
 
-   '(button ((t (:inherit (link)))))
-   `(link ((t (:foreground ,yozora-star :underline t))))
+   '(button       ((t (:inherit (link)))))
+   `(link         ((t (:foreground ,yozora-star :underline t))))
    `(link-visited ((t (:inherit link :foreground ,yozora-cloud))))
-   `(fringe ((t (:background ,yozora-sky))))
-   `(header-line ((t (:inherit mode-line :background ,yozora-sky :foreground ,yozora-wing :box nil))))
-   `(tooltip ((t (:inherit variable-pitch :background ,yozora-meteor :foreground ,yozora-bird))))
+   `(fringe       ((t (:background ,yozora-sky))))
+   `(header-line  ((t (:inherit mode-line :background ,yozora-sky :foreground ,yozora-wing :box nil))))
+   `(tooltip      ((t (:inherit variable-pitch :background ,yozora-meteor :foreground ,yozora-bird))))
 
-   `(isearch ((t (:background ,yozora-leaf :foreground ,yozora-bird))))
-   `(isearch-fail ((t (:foreground ,yozora-bird :background ,yozora-heart))))
-   `(lazy-highlight ((t (:background ,yozora-sky))))
-   `(match ((t (:foreground ,yozora-bird :background ,yozora-star))))
-   '(next-error ((t (:inherit (region)))))
-   '(query-replace ((t (:inherit isearch))))
+   `(isearch          ((t (:background ,yozora-leaf :foreground ,yozora-bird))))
+   `(isearch-fail     ((t (:foreground ,yozora-bird :background ,yozora-heart))))
+   `(lazy-highlight   ((t (:background ,yozora-sky))))
+   `(match            ((t (:foreground ,yozora-bird :background ,yozora-star))))
+   '(next-error       ((t (:inherit (region)))))
+   '(query-replace    ((t (:inherit isearch))))
    `(info-index-match ((t (:background ,yozora-bird))))
 
-   `(web-mode-doctype-face ((t (:foreground ,yozora-cloud))))
-   `(web-mode-html-tag-face ((t (:foreground ,yozora-leaf))))
-   `(web-mode-html-attr-name-face ((t (:foreground ,yozora-star))))
-   `(web-mode-html-attr-value-face ((t (:foreground ,yozora-moon))))
-   `(web-mode-html-tag-bracket-face ((t (:foreground ,yozora-leaf))))
-   `(web-mode-css-selector-face ((t (:foreground ,yozora-leaf))))
+   `(web-mode-doctype-face           ((t (:foreground ,yozora-cloud))))
+   `(web-mode-html-tag-face          ((t (:foreground ,yozora-leaf))))
+   `(web-mode-html-attr-name-face    ((t (:foreground ,yozora-star))))
+   `(web-mode-html-attr-value-face   ((t (:foreground ,yozora-moon))))
+   `(web-mode-html-tag-bracket-face  ((t (:foreground ,yozora-leaf))))
+   `(web-mode-css-selector-face      ((t (:foreground ,yozora-leaf))))
    `(web-mode-css-property-name-face ((t (:foreground ,yozora-leaf))))
-   `(web-mode-function-name-face ((t (:foreground ,yozora-heart))))
-   `(web-mode-function-call-face ((t (:foreground ,yozora-star))))
-   `(web-mode-valiable-name-face ((t (:foreground ,yozora-leaf))))))
+   `(web-mode-function-name-face     ((t (:foreground ,yozora-heart))))
+   `(web-mode-function-call-face     ((t (:foreground ,yozora-star))))
+   `(web-mode-valiable-name-face     ((t (:foreground ,yozora-leaf))))))
 
 ;;;###autoload
 (when (and (boundp 'custom-theme-load-path) load-file-name)
